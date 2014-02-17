5381cfa66f61567361bfbb7998ba311bd498d30f
Oskar <oskar.georgsson@gmail.com>
2014-02-17 23:49:19 +0100

    Delete resource and error messages

--- WebAPI/TOERHWebAPI/app/controllers/api/v1/resource_controller.rb
+++ WebAPI/TOERHWebAPI/app/controllers/api/v1/resource_controller.rb
@@ -8,9 +8,10 @@ class Api::V1::ResourceController < ApplicationController
 	def index
 		resources = Resource.all
 		if resource != nil
-			@resultArray = Array.new
+			resultArray = Array.new
 			resources.each do |resource|
 				resourceHash = Hash.new
+				resourceHash["statuscode"]=200
 				resourceHash["resource_id"]=resource.id
 				resourceHash["resource_type_id"]=resource.resource_type_id
 				resourceHash["resource_type"]=resource.resource_type
@@ -21,11 +22,11 @@ class Api::V1::ResourceController < ApplicationController
 				resourceHash["description"]=resource.description
 				resourceHash["url"]=resource.url
 				resourceHash["tags"]=resource.tags
-				@resultArray << resourceHash
+				resultArray << resourceHash
 			end
 			respond_to do |f|
-				f.json { render json: @resultArray, :status => 200 }
-				f.xml { render xml: @resultArray, :status => 200 }
+				f.json { render json: resultArray, :status => 200 }
+				f.xml { render xml: resultArray, :status => 200 }
 			end
 		else
 			respond_to do |f|
@@ -41,6 +42,7 @@ class Api::V1::ResourceController < ApplicationController
 			resource = Resource.find(params[:id])
 			
 			resourceHash = Hash.new
+			resourceHash["statuscode"]=200
 			resourceHash["resource_id"]=resource.id
 			resourceHash["resource_type_id"]=resource.resource_type_id
 			resourceHash["resource_type"]=resource.resource_type
@@ -57,40 +59,66 @@ class Api::V1::ResourceController < ApplicationController
 				f.xml { render xml: resourceHash, :status => 200 }
 			end
 		rescue
+			errorHash = Hash.new
+			errorHash["statuscode"] = 404
+			errorHash["Errormessage"] = "Resource not found"
 			respond_to do |f|
-				f.json { render json: "The requested resource was not found", :status => 404 }
-				f.xml { render xml: "The requested resource was not found", :status => 404 }
+				f.json { render json: errorHash, :status => 404 }
+				f.xml { render xml: errorHash, :status => 404 }
 			end
 		end
 	end
 	
-	# POST: api/v1/resource?apikey=yourAPIKey
+	# Comment:
+		# POST: api/v1/resource?apikey=yourAPIKey
+		# JSON: {
+		#   "resource": {
+		#     "name":"Name of resource",
+		#     "resourcetype":"Resource type",
+		#     "licencetype":"Licence type",
+		#     "username":"Owner of resource"
+		#     "description":"Resource description",
+		#     "url":"Resource location",
+		#     "tags":[Array of resource tags]
+		#   }
+		# }
 	def create
 		if params[:resource] != nil
 			resourcetype = ResourceType.find_or_create_by_resource_type(params[:resource][:resourcetype])
 			licencetype = Licence.find_or_create_by_licence_type(params[:resource][:licencetype])
 			description = params[:resource][:description]
+			name = params[:resource][:name]
 			url = params[:resource][:url]
 			tags = params[:resource][:tags]
-			user = User.find_by_username(params[:username])
+			user = User.find_by_username(params[:resource][:username])
 			
 			resource = Resource.new
+			resource.name = name
 			resource.resource_type_id = resourcetype.id
 			resource.user_id = user.id
 			resource.licence_id = licencetype.id
 			resource.description = description
 			resource.url = url
-			resource.created_at = DateTime.now.to_date
-			resource.updated_at = DateTime.now.to_date
+			resource.created_at = DateTime.now
+			resource.updated_at = DateTime.now
 			resource.save
 			
 			tags.each do |tag|
 				tagInfo = Tag.find_or_create_by_tag(tag)
 				resource.tags << tagInfo
 			end
-			render :json => resource, :status => 201
+			respond_to do |f|
+				f.json { render json: resource, :status => 201 }
+				f.xml { render xml: resource, :status => 201 }
+			end
 		else
-			render :json => "", :status => 400
+			errorHash = Hash.new
+			errorHash["statuscode"] = 404
+			errorHash["Errormessage"] = "Check your JSON body resource parameter not found"
+			respond_to do |f|
+				f.json { render json: errorHash, :status => 404 }
+				f.xml { render xml: errorHash, :status => 404 }
+			end
 		end
 	end
 	
@@ -101,31 +129,49 @@ class Api::V1::ResourceController < ApplicationController
 	
 	# DELETE: api/v1/resource/:id?apikey=yourAPIKey
 	def destroy
-		Resource.find(params[:id]).destroy
-		render :nothing => true, :status => 204
+		begin
+			resource = Resource.find(params[:id])
+			user = User.find_by_username(params[:username])
+			if resource.user_id == user.id
+				resource.destroy
+				respond_to do |f|
+					f.json { render :nothing => true, :status => 204 }
+					f.xml { render :nothing => true, :status => 204 }
+				end
+			else
+				errorHash = Hash.new
+				errorHash["statuscode"] = 403
+				errorHash["Errormessage"] = "User unauthorized"
+				render :json => errorHash, :status => 403
+			end
+		rescue
+			errorHash = Hash.new
+			errorHash["statuscode"] = 404
+			errorHash["Errormessage"] = "Resource not found"
+			render :json => errorHash, :status => 404
+		end
 	end
 	
 	private
-	def validateApiKey
-		key = ApiKey.find_by key: params[:apikey]
-		if key == nil
-			errorHash = Hash.new
-			errorHash["statuscode"] = 401
-			errorHash["Errormessage"] = "Application apikey is missing or it's wrong"
-			errorHash["Fix"] = "Check your apikey and correct if wrong"
-			respond_to do |f|
-				f.json { render json: errorHash, :status => 401 }
-				f.xml { render xml: errorHash, :status => 401 }
+		def validateApiKey
+			key = ApiKey.find_by key: params[:apikey]
+			if key == nil
+				errorHash = Hash.new
+				errorHash["statuscode"] = 401
+				errorHash["Errormessage"] = "Application apikey is missing or it's wrong"
+				respond_to do |f|
+					f.json { render json: errorHash, :status => 401 }
+					f.xml { render xml: errorHash, :status => 401 }
+				end
+				return false
+			else
+				return true
 			end
-			return false
-		else
-			return true
 		end
-	end
-	
-	def validateUser
-		authenticate_or_request_with_http_basic do |username, password|
-			User.find_by_username(username).password == Digest::SHA512.hexdigest(password) && params[:username] == username
+		
+		def validateUser
+			authenticate_or_request_with_http_basic do |username, password|
+				User.find_by_username(username).password == Digest::SHA512.hexdigest(password) && params[:username] == username
+			end
 		end
-	end
 end
