b28dcdcf0adb805ee0ddbe33ef44569cda9865f8
Oskar <oskar.georgsson@gmail.com>
2014-03-20 23:39:27 +0100

    rails seed update

---
+++ ClientApplication/README.md
@@ -0,0 +1,8 @@
+För att kunna köra applikationen så måste du gå igenom ett par steg:
+
+1. Öppna en terminal och gå till mappen där rails finns och kör "bundle install"
+2. Kör sedan "rake db:migrate"
+3. Kör sedan "rake db:seeds"
+4. Starta rails servern
+5. Öppna sedan Klientmappen som en "Web site" i Visual studio <2012
+6. Bara å testa applikationen.
--- WebAPI/TOERHWebAPI/db/seeds.rb
+++ WebAPI/TOERHWebAPI/db/seeds.rb
@@ -5,3 +5,81 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Application
+#
+Application.delete_all
+
+app1 = Application.new
+app1.contact_mail = "test@testmail.se"
+app1.application_name="Testapp"
+app1.save
+
+# ApiKey
+ApiKey.delete_all
+
+ak = ApiKey.new
+ak.key = "s4ciD75L69UAXz0y8QrhJfbNVOm3T21wGkpe"
+ak.Application_id=app1.id
+ak.save
+
+
+
+#Licence
+Licence.delete_all
+
+l = Licence.new
+l.licence_type="Attribution CC BY"
+l.save
+
+l = Licence.new
+l.licence_type="Attribution-ShadeAlike CC BY-SA"
+l.save
+
+l = Licence.new
+l.licence_type="Attribution-NoDerivs CC BY-ND"
+l.save
+
+l = Licence.new
+l.licence_type="Attribution-NonCommercial CC BY-NC"
+l.save
+
+l = Licence.new
+l.licence_type="Attribution-NonCommercial-ShareAlike CC BY-NC-SA"
+l.save
+
+l = Licence.new
+l.licence_type="Attribution-NonCommercial-ShareAlike CC BY-NC-ND"
+l.save
+
+l = Licence.new
+l.licence_type="GNU"
+l.save
+
+l = Licence.new
+l.licence_type="MIT"
+l.save
+
+# ResourceType
+ResourceType.delete_all
+
+rt = ResourceType.new
+rt.resource_type="Picture"
+rt.save
+
+rt = ResourceType.new
+rt.resource_type="Media"
+rt.save
+
+rt = ResourceType.new
+rt.resource_type="Video"
+rt.save
+
+rt = ResourceType.new
+rt.resource_type="PDF"
+rt.save
+
+
+
+
+
